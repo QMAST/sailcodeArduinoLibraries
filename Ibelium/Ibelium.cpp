5ae9a57f344ef29349565d5672305cfae0a1ee94
Kevin Gibson <rkevingibson@gmail.com>
2013-02-07 19:21:14 -0500

    Updated Ibelium library

--- Ibelium/Ibelium.cpp
+++ Ibelium/Ibelium.cpp
@@ -1,44 +1,89 @@
-#include <"Ibelium.h">
+#include "Arduino.h"
+#include "Ibelium.h"
 //Each function returns 1 if successful, 0 otherwise.
 
-
-int Ibelium::sendATCmd(char* cmd) {
-	char* buff;
-	int size;
-	Serial.print(cmd);
-	Serial.print((char)0xD);
-	Serial.print((char)0xA);
-	Serial.flush();
-	delay(2000);
-	if(Serial.available()>0)
-	{
-		size = Serial.available();
-		buff= (char*) malloc(sizeof(char) * Serial.available());
-		Serial.readBytes(buff, size);
-	}
-
+Ibelium::Ibelium() {
+	//Empty Constructor
 }
 
-int Ibelium::sendATQuery(char* cmd, char* response) {
-
-}
-
-int Ibelium::sendSMS(char* message, char* response) {
-
-}
-
-int init() {
+int Ibelium::init() {
 	//Initializes and tests the module. 
 	Serial.begin(115200);
 	delay(2000);
 	pinMode(led, OUTPUT);
 	pinMode(onModulePin, OUTPUT);
 	switchModule(); //Switches the module on
-
 	return sendATCmd("AT");
 }
 
-void switchModule() {
+int Ibelium::sendATCmd(char* cmd) {
+	//Send an AT Command without returning the response
+	char* resp;
+	int s;
+	s = sendATQuery(cmd, resp);
+	free(resp);
+	return s;
+}
+
+int Ibelium::sendATQuery(char* cmd, char* response) {
+	//Send an AT command, and get the response from the board.
+
+	free(response); //Avoid creating a memory leak.
+	_error=0;
+	_watchdog=0;
+
+	int size=10;
+	char* buff = (char*) calloc(size, sizeof(char)); //Allocate a 10 character buffer for the response
+	int x=0;
+	Serial.print(cmd);
+	Serial.print((char)0xD);
+	Serial.print((char)0xA);
+	Serial.flush();
+	delay(2000);
+	//By now, the command should be sent completely.
+
+	do {
+		_watchdog=0;
+		while(Serial.available()==0 && _watchdog<1000) _watchdog++;
+
+		if(_watchdog>=1000) //No response received after a certain length of time
+		{
+			_error=2;
+			break;
+		}
+
+
+		buff[x]=Serial.read(); //Read the next character
+		x++;
+
+		if(x>=size) { //Reallocate memory if message is getting longer.
+			size *= 2;
+			buff = (char*) realloc(buff, size);
+		}
+
+		if(size>256){ //Reponse too long
+			_error = 1;
+			break;
+		}
+
+	} while(!(buff[x-1]=='K' && buff[x-2]=='O'));//Keep Waiting for a response until the last two characters are 'OK'
+
+	response = buff;
+
+	if(_error!=0)
+		return 1;
+	else 
+		return 0;	
+}
+
+int Ibelium::sendSMS(char* message, char* response) {
+	
+	
+	return 1;
+}
+
+
+void Ibelium::switchModule() {
     digitalWrite(onModulePin,HIGH);
     delay(2000);
     digitalWrite(onModulePin,LOW);
--- Ibelium/Ibelium.h
+++ Ibelium/Ibelium.h
@@ -2,12 +2,13 @@
 messages, uploading log files to an ftp server, and receiving text messages.
 
 
+
+
 */
 #ifndef Ibelium_h
 #define Ibelium_h
 
-#include <Arduino.h>
-#include <SD.h>
+#include "Arduino.h"
 
 #define onModulePin 2
 #define led 13
@@ -15,13 +16,25 @@ messages, uploading log files to an ftp server, and receiving text messages.
 class Ibelium
 {
 public:
+	Ibelium();
 	int init();
-	int sendSMS(char* message);
-
-private:
+	int sendSMS(char* message, char* response);
 	int sendATCmd(char* cmd);
+	//char* checkError(); Not implemented yet
+private:
+	int sendATQuery(char* cmd, char* response);
 	void switchModule();
+
+	int _error; //An error code
+	int _watchdog; //A watchdog counter, for responses
 };
 
 
+/*
+Error code mapping:
+1 - Invalid or null response
+
+2 - Response overflow
+
+*/
 #endif
