35d679019404be03ed7dbea0b4aee1626e315eff
Kevin Gibson <rkevingibson@gmail.com>
2013-02-10 15:24:47 -0500

    Quick upload of the library

--- Ibelium/Ibelium.cpp
+++ Ibelium/Ibelium.cpp
@@ -10,10 +10,12 @@ int Ibelium::init() {
 	//Initializes and tests the module. 
 	Serial.begin(115200);
 	delay(2000);
-	pinMode(led, OUTPUT);
-	pinMode(onModulePin, OUTPUT);
+	pinMode(LED, OUTPUT);
+	pinMode(ON_MODULE_PIN, OUTPUT);
 	switchModule(); //Switches the module on
-	return sendATCmd("AT");
+
+	//Send SMS mode to Text
+	return sendATCmd("AT+CMGF=1");
 }
 
 int Ibelium::sendATCmd(char* cmd) {
@@ -28,7 +30,7 @@ int Ibelium::sendATCmd(char* cmd) {
 int Ibelium::sendATQuery(char* cmd, char* response) {
 	//Send an AT command, and get the response from the board.
 
-	//free(response); //Avoid creating a memory leak.
+	free(response); //Avoid creating a memory leak.
 	_error=0;
 	_watchdog=0;
 
@@ -36,6 +38,66 @@ int Ibelium::sendATQuery(char* cmd, char* response) {
 	char* buff = (char*) calloc(size, sizeof(char)); //Allocate a 10 character buffer for the response
 	int x=0;
 	Serial.print(cmd);
+	Serial.print((char)0xD);
+	Serial.print((char)0xA);
+	Serial.flush();
+	delay(2000);
+	//By now, the command should be sent completely.
+
+	do {
+		_watchdog=0;
+		while(Serial.available()==0 && _watchdog<1000) _watchdog++;
+
+		if(_watchdog>=1000) //No response received after a certain length of time
+		{
+			Serial.println("Watchdog timer gone");
+			_error=2;
+			break;
+		}
+
+
+		buff[x]=Serial.read(); //Read the next character
+		x++;
+
+		if(x>=size) { //Reallocate memory if message is getting longer.
+			size *= 2;
+			buff = (char*) realloc(buff, size);
+		}
+
+		if(size>256){ //Reponse too long
+			_error = 1;
+			//Serial.println(size);
+			break;
+		}
+
+	} while(x<2 || !(buff[x-1]=='K' && buff[x-2]=='O')  );//Keep Waiting for a response until the last two characters are 'OK'
+
+	buff[x]=0x0;
+	response = buff;
+
+	if(_error==0)
+		return x;
+	else 
+		return 0;	
+}
+
+int Ibelium::sendSMS(char* number, char* message, char* response) {
+	//Sends an AT Command to send an SMS message
+	//AT+CMGS="number"<CR> message <ESC>
+	 //Initial attempt, ugly as fuck
+/*	free(response); //Avoid creating a memory leak.
+	_error=0;
+	_watchdog=0;
+
+	int size=10;
+	char* buff = (char*) calloc(size, sizeof(char)); //Allocate a 10 character buffer for the response
+	int x=0;
+	Serial.print("AT+CMGS=\"");
+	Serial.print(number);
+	Serial.print("\"");
+	Serial.print((char)0xD);
+	Serial.print(message);
+	Serial.print((char)0x1B);
 	Serial.print((char)0xD);
 	Serial.print((char)0xA);
 	Serial.flush();
@@ -76,20 +138,53 @@ int Ibelium::sendATQuery(char* cmd, char* response) {
 	if(_error==0)
 		return x;
 	else 
-		return 0;	
+		return 0;
+
+		*/
+
+	//Cleaner attempt
+	char* cmd = appendStrings("AT+CMGS=\"", number);
+	cmd = appendStrings(cmd, "\"\r");
+	cmd = appendStrings(cmd, message);
+	cmd = appendStrings(cmd, "\x1b");
+
+	return sendATQuery(cmd, response);
 }
 
-int Ibelium::sendSMS(char* message, char* response) {
-	
-	
-	return 1;
-}
+int Ibelium::readSMS(char* ) {
+	//Reads the most recent SMS and 
+	//char*
 
+
+	return 0; 
+}
 
 void Ibelium::switchModule() {
-    digitalWrite(onModulePin,HIGH);
+    digitalWrite(ON_MODULE_PIN,HIGH);
     delay(2000);
-    digitalWrite(onModulePin,LOW);
+    digitalWrite(ON_MODULE_PIN,LOW);
 }
 
+char* appendStrings(char* a, char* b) {
+	int lenA, lenB;
+	char* newString;
+	lenA=0;
+	lenB=0;
+	//Find the length of the two strings;
+	while(a[lenA]!='\0') lenA++;
+	while(b[lenB]!='\0') lenB++;
 
+	newString = (char*) malloc(sizeof(char)*(lenA+lenB+1));
+
+	for(int i=0; i<lenA; i++) {
+		newString[i] = a[i];
+	}
+	for(int i=0; i<lenB; i++) {
+		newString[lenA+i] = b[i];
+	}
+	newString[lenA+lenB]='\0';
+	free(a);
+	free(b);
+
+	return newString;
+}
--- Ibelium/Ibelium.h
+++ Ibelium/Ibelium.h
@@ -1,6 +1,19 @@
 /*  Ibelium 3G module library.  Allows for sending messages through text
 messages, uploading log files to an ftp server, and receiving text messages.
 
+AT Commands: all prefixed by AT+
+
+CMGL="ALL" - lists all messages currently stored (incoming and outgoing)
+
+CPMS="SM","SM","SM" to select SMS storage
+
+CMGR=0 - reads first message in the SMS storage
+
+CMGF=1 - sets text mode
+
+CMGS="+1number"<CR>
+message goes here <ctrl-z>
+^sends a text message
 
 */
 #ifndef Ibelium_h
@@ -8,21 +21,22 @@ messages, uploading log files to an ftp server, and receiving text messages.
 
 #include "Arduino.h"
 
-#define onModulePin 2
-#define led 13
 
 class Ibelium
 {
 public:
 	Ibelium();
 	int init();
-	int sendSMS(char* message, char* response);
+	int sendSMS(char* number, char* message, char* response);
 	int sendATCmd(char* cmd);
 	int sendATQuery(char* cmd, char* response);
+	int readSMS();
 	//char* checkError(); Not implemented yet
 private:	
 	void switchModule();
-
+	char* appendStrings(char* a, char* b);
+	static const int ON_MODULE_PIN =2;
+	static const int LED = 13;
 	int _error; //An error code
 	int _watchdog; //A watchdog counter, for responses
 };
